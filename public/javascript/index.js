68b88127103f8b306f8fdd5acc863b0d1589b731
Sneha Mahajan <sneh.km@gmail.com>
2022-07-28 17:25:56 -0400

    Add setColors function to change colour of span via text in span.

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,7 +1,11 @@
 $(() => {
-  const testJquery = () => {
-    $("#main").addClass("makeRed");
-  }
-
-  testJquery();
+  const setColours = () => {
+    $(".combination")
+      .children("span")
+      .each(function () {
+        const colorValue = $(this).text();
+        $(this).css("background-color", colorValue);
+      });
+  };
+  setColours();
 });
