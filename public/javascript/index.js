4114fccec9f793fc9caf9a7f1c618ccc461b0c67
Sneha Mahajan <sneh.km@gmail.com>
2022-08-01 15:18:26 -0400

    Change display of text color when hovering over colours.

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,27 +1,36 @@
 $(() => {
-  const checkDarkness = (colour) => {
+  function checkDarkness(colour) {
     const c = colour.substring(1); // strip #
     const rgb = parseInt(c, 16); // convert rrggbb to decimal
     const r = (rgb >> 16) & 0xff; // extract red
     const g = (rgb >> 8) & 0xff; // extract green
     const b = (rgb >> 0) & 0xff; // extract blue
-
     const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
-    if (luma < 80) {
-      return true;
-    }
+    if (luma < 80) return true;
     return false;
-  };
+  }
 
-  const setColours = () => {
+  function setColours () {
     $(".combination")
       .children("span")
       .each(function () {
-        const colorValue = $(this).text().trim();
-        const isDark = checkDarkness(colorValue);
-        isDark ? $(this).css("color", "#FFF") : $(this).css("olor", "#000");
+        const colorValue = `#${$(this).text().trim()}`;
+        $(this).css("color", colorValue);
         $(this).css("background-color", colorValue);
       });
   };
   setColours();
+
+  function handlerIn() {
+    const colorValue = "#" + $(this).text().trim();
+    const isDark = checkDarkness(colorValue);
+    isDark ? $(this).css("color", "#FFF") : $(this).css("color", "#000");
+  }
+
+  function handlerOut() {
+    const colorValue = `#${$(this).text().trim()}`;
+    $(this).css("color", colorValue);
+  }
+
+  $(".colour").mouseenter(handlerIn).mouseleave(handlerOut);
 });
