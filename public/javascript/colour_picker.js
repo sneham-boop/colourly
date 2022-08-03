305818a34c30243abfc82f552f4dcf81076478d5
Sneha Mahajan <sneh.km@gmail.com>
2022-08-03 13:54:20 -0400

    Add logic for picking a new colour.

---
+++ public/javascript/colour_picker.js
@@ -0,0 +1,85 @@
+let colorBlock = document.getElementById('color-block');
+let ctx1 = colorBlock.getContext('2d');
+let width1 = colorBlock.width;
+let height1 = colorBlock.height;
+
+let colorStrip = document.getElementById('color-strip');
+let ctx2 = colorStrip.getContext('2d');
+let width2 = colorStrip.width;
+let height2 = colorStrip.height;
+
+let colorLabel = document.getElementById('color-label');
+
+let x = 0;
+let y = 0;
+let drag = false;
+let rgbaColor = 'rgba(174,32,18,1)';
+
+ctx1.rect(0, 0, width1, height1);
+fillGradient();
+
+ctx2.rect(0, 0, width2, height2);
+let grd1 = ctx2.createLinearGradient(0, 0, 0, height1);
+grd1.addColorStop(0, 'rgba(255, 0, 0, 1)');
+grd1.addColorStop(0.17, 'rgba(255, 255, 0, 1)');
+grd1.addColorStop(0.34, 'rgba(0, 255, 0, 1)');
+grd1.addColorStop(0.51, 'rgba(0, 255, 255, 1)');
+grd1.addColorStop(0.68, 'rgba(0, 0, 255, 1)');
+grd1.addColorStop(0.85, 'rgba(255, 0, 255, 1)');
+grd1.addColorStop(1, 'rgba(255, 0, 0, 1)');
+ctx2.fillStyle = grd1;
+ctx2.fill();
+
+function click(e) {
+  x = e.offsetX;
+  y = e.offsetY;
+  let imageData = ctx2.getImageData(x, y, 1, 1).data;
+  rgbaColor = 'rgba(' + imageData[0] + ',' + imageData[1] + ',' + imageData[2] + ',1)';
+  fillGradient();
+}
+
+function fillGradient() {
+  ctx1.fillStyle = rgbaColor;
+  ctx1.fillRect(0, 0, width1, height1);
+
+  let grdWhite = ctx2.createLinearGradient(0, 0, width1, 0);
+  grdWhite.addColorStop(0, 'rgba(255,255,255,1)');
+  grdWhite.addColorStop(1, 'rgba(255,255,255,0)');
+  ctx1.fillStyle = grdWhite;
+  ctx1.fillRect(0, 0, width1, height1);
+
+  let grdBlack = ctx2.createLinearGradient(0, 0, 0, height1);
+  grdBlack.addColorStop(0, 'rgba(0,0,0,0)');
+  grdBlack.addColorStop(1, 'rgba(0,0,0,1)');
+  ctx1.fillStyle = grdBlack;
+  ctx1.fillRect(0, 0, width1, height1);
+}
+
+function mousedown(e) {
+  drag = true;
+  changeColor(e);
+}
+
+function mousemove(e) {
+  if (drag) {
+    changeColor(e);
+  }
+}
+
+function mouseup(e) {
+  drag = false;
+}
+
+function changeColor(e) {
+  x = e.offsetX;
+  y = e.offsetY;
+  let imageData = ctx1.getImageData(x, y, 1, 1).data;
+  rgbaColor = 'rgba(' + imageData[0] + ',' + imageData[1] + ',' + imageData[2] + ',1)';
+  colorLabel.style.backgroundColor = rgbaColor;
+}
+
+colorStrip.addEventListener("click", click, false);
+
+colorBlock.addEventListener("mousedown", mousedown, false);
+colorBlock.addEventListener("mouseup", mouseup, false);
+colorBlock.addEventListener("mousemove", mousemove, false);
