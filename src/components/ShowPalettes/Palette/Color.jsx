6296a591f764eb0c6bbf8f852dcd4026d7491077
Sneha Mahajan <sneh.km@gmail.com>
2023-04-17 16:03:18 -0400

    Copy colour value when clicked.

--- src/components/ShowPalettes/Palette/Color.jsx
+++ src/components/ShowPalettes/Palette/Color.jsx
@@ -1,9 +1,23 @@
 import styles from "../ShowPalettes.module.scss";
 
 export default function Color({ color }) {
+  const handleClick = (e) => {
+    const colourValue = e.target.innerHTML;
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(colourValue).then(() => {
+        console.log("I copied!!!");
+      });
+    } else {
+      console.log("Browser Not compatible");
+    }
+  };
   return (
     <>
-      <span className={styles.colour} style={{ backgroundColor: `${color}` }}>
+      <span
+        className={styles.colour}
+        style={{ backgroundColor: `${color}` }}
+        onClick={(e) => handleClick(e)}
+      >
         {color}
       </span>
     </>
