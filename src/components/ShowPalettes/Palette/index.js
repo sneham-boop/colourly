887606aa2e4f561f472c0f03f30297054790c603
Sneha Mahajan <sneh.km@gmail.com>
2023-04-17 19:03:04 -0400

    Add menu for palette.

--- src/components/ShowPalettes/Palette/index.js
+++ src/components/ShowPalettes/Palette/index.js
@@ -1,11 +1,38 @@
 import Color from "./Color";
 import styles from "../ShowPalettes.module.scss";
+import { useState } from "react";
+
+function Menu() {
+  return (
+    <ul>
+      <li>
+        <span className="material-symbols-rounded">open_in_full</span>
+        <p>View fullscreen</p>
+      </li>
+      <li>
+        <span className="material-symbols-rounded">favorite</span>
+        <span>Unsave</span>
+      </li>
+      <li>
+        <span className="material-symbols-rounded">delete</span>
+        <span>Delete</span>
+      </li>
+    </ul>
+  );
+}
 
 export default function Palette({ palette }) {
+  const [showMenu, setShowMenu] = useState(false);
   const showColours = (palette) => {
     return palette.map((colour, id) => {
       return <Color key={id} color={`#${colour}`} />;
     });
+  };
+  const handleClick = (e) => {
+    setShowMenu(true);
+  };
+  const handleMouseDown = (e) => {
+    setShowMenu(false);
   };
   return (
     <>
@@ -14,7 +41,14 @@ export default function Palette({ palette }) {
         <div className={styles["combination-info"]}>
           <span className="material-symbols-rounded">favorite</span>
           <p className={styles["likes"]}>6786</p>
-          <span className="material-symbols-rounded">more_horiz</span>
+          <span
+            className="material-symbols-rounded"
+            onClick={(e) => handleClick(e)}
+            onMouseDown={(e) => handleMouseDown(e)}
+          >
+            more_horiz
+          </span>
+          {showMenu && <Menu />}
         </div>
       </div>
     </>
