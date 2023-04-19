3d0a2686f3fc7c8e879675b8944b60b7daf7ef45
Sneha Mahajan <sneh.km@gmail.com>
2023-04-19 16:50:14 -0400

    Fix footer styling.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import Button from "../Button";
 import styles from "./Footer.module.scss";
+import Heart from "@mui/icons-material/FavoriteRounded";
 
 export default function Footer() {
   const btnText = "GO UP";
@@ -12,7 +13,18 @@ export default function Footer() {
 
   return (
     <footer className={styles.footer}>
-      <Button btnText={btnText} onClick={() => handleScroll()} />
+      <section className={styles["made-by"]}>
+        <span>Made with </span>
+        <span>
+          <Heart sx={{ color: "#F9684F" }} />
+        </span>
+        <span>by Sneha Mahajan</span>
+      </section>
+      <p className={styles["coolers-text"]}>
+        Inspired by the amazing tool created by{" "}
+        <a href="https://coolors.co/palettes/trending">Coolers</a>
+      </p>
+      <Button btnText={btnText} onClick={() => handleScroll()} custom="go-up"/>
     </footer>
   );
 }
